348fa0c192a7054b5a7308a8bb72bc686e98c9cc
ds <michael@draftspot.net>
2020-07-13 19:57:06 +0200

    prvni verze

---
+++ wp-content/themes/yourpraguetransfer/header.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <!-- Main CSS -->
+        <link rel="stylesheet" href="../yourpraguetransfer/wp-content/themes/yourpraguetransfer/style.css">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        
+        
+        <!-- Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <title>Your Prague Transfers</title>
+        
+        <!--Font Awesome-->
+        <link href="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/FontAwesome/fontawesome-free-5.13.1-web/css/fontawesome.css" rel="stylesheet">
+        <link href="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/FontAwesome/fontawesome-free-5.13.1-web/css/brands.css" rel="stylesheet">
+        <link href="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/FontAwesome/fontawesome-free-5.13.1-web/css/solid.css" rel="stylesheet">
+        
+        <!-- Arrows animation -->
+        <script src="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/js/arrow.js"></script>
+        
+        <!-- Justified Gallery -->
+        <link rel="stylesheet" href="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/JustifiedGallery/justifiedGallery.min.css">
+        <script src="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/JustifiedGallery/jquery.justifiedGallery.min.js"></script>
+        
+    </head>
+    <body>
+        <header class="container-fluid bg-white fixed-top px-0">
+            <nav class="s7_sw-sec navbar navbar-expand-lg mx-auto"> <!-- Bootstrap sticky responsive header -->
+                <a href="/" class="navbar-brand"><img src="../yourpraguetransfer/wp-content/themes/yourpraguetransfer/assets/images/page-logo.png" alt="logo-icon-car"></a> <!-- Logo -->
+                <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button> <!-- Burger Icon -->
+                <div id="navbarCollapse" class="navbar-collapse collapse"> <!-- Responsive menu -->
+                    <ul class="navbar-nav align-items-center ml-auto">
+                        <li class="nav-item active">
+                            <a href="/" class="s7_color-1 nav-link text-uppercase">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="s7_color-1 nav-link text-uppercase">O nás</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="s7_color-1 nav-link text-uppercase">Ceník</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="s7_color-1 nav-link text-uppercase">Výlety</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="s7_color-1 nav-link text-uppercase">Bezpečnost</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="s7_color-1 nav-link text-uppercase">Kontakt</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="tel:777 888 999" class="nav-link font-weight-bold ml-2">
+                                <i class="fas fa-phone-alt text-white"></i>
+                                <span class="mx-2 text-white">|</span>
+                                <span class="text-white">777 888 999</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <div id="content" class="s7_site-content"> <!-- Page Content START -->
