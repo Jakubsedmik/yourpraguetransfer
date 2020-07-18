5d0c2a2f46b79c90f106e10e6f82c2dd9a3843d5
Jakubsedmik <info@studioseven.cz>
2020-07-18 17:39:49 +0200

    FIRST DEPLOY

--- wp-content/themes/yourpraguetransfer/header.php
+++ wp-content/themes/yourpraguetransfer/header.php
@@ -5,30 +5,32 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         
-        <!-- Main CSS -->
-        <link rel="stylesheet" href="../yourpraguetransfer/wp-content/themes/yourpraguetransfer/style.css">
+
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        
-        
+
         <!-- Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <title>Your Prague Transfers</title>
-        
+
+
+        <!-- Main CSS -->
+        <link rel="stylesheet" href="<?php echo home_url(); ?>/wp-content/themes/yourpraguetransfer/style.css">
+
         <!--Font Awesome-->
-        <link href="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/FontAwesome/fontawesome-free-5.13.1-web/css/fontawesome.css" rel="stylesheet">
-        <link href="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/FontAwesome/fontawesome-free-5.13.1-web/css/brands.css" rel="stylesheet">
-        <link href="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/FontAwesome/fontawesome-free-5.13.1-web/css/solid.css" rel="stylesheet">
+        <link href="<?php echo home_url(); ?>/wp-content/themes/yourpraguetransfer/assets/FontAwesome/fontawesome-free-5.13.1-web/css/fontawesome.css" rel="stylesheet">
+        <link href="<?php echo home_url(); ?>/wp-content/themes/yourpraguetransfer/assets/FontAwesome/fontawesome-free-5.13.1-web/css/brands.css" rel="stylesheet">
+        <link href="<?php echo home_url(); ?>/wp-content/themes/yourpraguetransfer/assets/FontAwesome/fontawesome-free-5.13.1-web/css/solid.css" rel="stylesheet">
         
         <!-- Arrows animation -->
-        <script src="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/js/arrow.js"></script>
+        <script src="<?php echo home_url(); ?>/wp-content/themes/yourpraguetransfer/assets/js/arrow.js"></script>
         
         <!-- Justified Gallery -->
-        <link rel="stylesheet" href="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/JustifiedGallery/justifiedGallery.min.css">
-        <script src="../yourpraguetransfer/wp-content\themes/yourpraguetransfer/assets/JustifiedGallery/jquery.justifiedGallery.min.js"></script>
+        <link rel="stylesheet" href="<?php echo home_url(); ?>/wp-content/themes/yourpraguetransfer/assets/JustifiedGallery/justifiedGallery.min.css">
+        <script src="<?php echo home_url(); ?>/wp-content/themes/yourpraguetransfer/assets/JustifiedGallery/jquery.justifiedGallery.min.js"></script>
         
     </head>
     <body>
