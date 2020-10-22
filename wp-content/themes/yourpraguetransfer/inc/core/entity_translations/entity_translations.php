f5116935928344753c602469a3d544200c2e8771
Jakubsedmik <info@studioseven.cz>
2020-10-22 21:38:47 +0200

    Dokončení překladů + drobné úpravy

--- wp-content/themes/yourpraguetransfer/inc/core/entity_translations/entity_translations.php
+++ wp-content/themes/yourpraguetransfer/inc/core/entity_translations/entity_translations.php
@@ -1,14 +1,19 @@
 <?php
-__('Mercedes Viano','yourpraguetransfer');
+__('Mercedes Vianoo','yourpraguetransfer');
  __('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies non elit nec pellentesque. Nunc consectetur odio dapibus urna auctor, vel posuere nisl hendrerit. ','yourpraguetransfer');
+ __('2','yourpraguetransfer');
  __('Lincoln TC120','yourpraguetransfer');
  __('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tristique sapien. Pellentesque et finibus lorem. Maecenas malesuada nulla ac sem lobortis gravida. In cursus quam neque, sagittis viverra risus pulvinar nec.','yourpraguetransfer');
+ __('3','yourpraguetransfer');
  __('Mercedes E','yourpraguetransfer');
  __('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tristique sapien. Pellentesque et finibus lorem. Maecenas malesuada nulla ac sem lobortis gravida. In cursus quam neque, sagittis viverra risus pulvinar nec.','yourpraguetransfer');
+ __('1','yourpraguetransfer');
  __('Ford','yourpraguetransfer');
  __('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris elementum mauris vitae tortor. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Vivamus luctus egestas leo. In dapibus augue non sapien. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.','yourpraguetransfer');
+ __('4','yourpraguetransfer');
  __('Škoda','yourpraguetransfer');
  __('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tristique sapien. Pellentesque et finibus lorem. Maecenas malesuada nulla ac sem lobortis gravida. In cursus quam neque, sagittis viverra risus pulvinar nec.','yourpraguetransfer');
+ __('0','yourpraguetransfer');
  __('Bussines Manager - Praha','yourpraguetransfer');
  __('Velice dobrá přepravní společnost, doporučuji vyzkoušet.','yourpraguetransfer');
  __('Frontend Developer','yourpraguetransfer');
