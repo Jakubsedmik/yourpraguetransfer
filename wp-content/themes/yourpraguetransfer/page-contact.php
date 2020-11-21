6a018b3d335c4369db336b6ec58688480edbf1ae
Jakubsedmik <info@studioseven.cz>
2020-11-21 19:21:04 +0100

    Oprava kontaktu

---
+++ wp-content/themes/yourpraguetransfer/page-contact.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+<section class="wrapper">
+
+
+    <?php
+    /* OBSAH JDOUCÍ Z POSTU */
+    if ( have_posts() ) {
+        while ( have_posts() ) {
+            ?>
+
+            <div class="wp-block-group container-fluid s7_us_sec1">
+                <div class="wp-block-group__inner-container">
+                    <div class="wp-block-group s7_sw-sec mx-auto">
+                        <div class="wp-block-group__inner-container">
+                            <h1 class="has-text-align-center font-weight-bold text-uppercase"><?php the_title(); ?></h1>
+                            <div class="s7_breadcrumbs">
+                                <?php the_breadcrumb(); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php
+
+            the_post();
+            the_content();
+
+            get_template_part("templates/page","contact");
+        }
+    }
+    ?>
+</section>
+
+<?php get_footer() ?>
